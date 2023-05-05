23766666b6ce52916dc9a0dc27f085e0214d81da
Amir Hamza <u2019071@giki.edu.pk>
2023-05-05 23:01:36 +0500

    asas

--- estore/src/app/app.module.ts
+++ estore/src/app/app.module.ts
@@ -1,6 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from'@angular/material/button';
+import {MatListModule,} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule,} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatMenuModule} from '@angular/material/menu';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +23,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,MatSidenavModule,MatToolbarModule,MatIconModule,MatButtonModule,MatListModule,MatCardModule,MatGridListModule,MatFormFieldModule,MatInputModule,MatTableModule,MatExpansionModule,MatSnackBarModule,MatBadgeModule,MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
